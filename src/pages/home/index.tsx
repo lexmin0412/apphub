2a10d9a9b03933f914267ff58a62e589c064f820
lexmin0412 <zhangle_dev@outlook.com>
2023-11-29 23:16:54 +0800

    feat: MVP

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,43 +1,98 @@
-import { useRequest } from 'ahooks'
-import { Button, message } from 'antd'
+import {Button, message} from "antd";
+import {useEffect} from "react";
+import fetch from "@toolkit-fe/request";
+import {useRequest} from "ahooks";
+import {Card} from "antd";
+const {Meta} = Card;
+import {Avatar, List} from "antd";
+import { RightOutlined }  from '@ant-design/icons'
+
+interface DataType {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  url: string;
+}
+
+interface Result {
+  list: DataType[];
+}
 
 function Home() {
-	const {
-		loading: fetchLoading,
-		runAsync: handleFakeFetch
-	} = useRequest(
-    () => {
-      return fetch(window.location.href).then((res) => {
-        return res.text();
+  const {loading: fetchLoading, data} = useRequest(() => {
+    return new Promise((resolve) => {
+      fetch(
+        "https://lexmin.oss-cn-hangzhou.aliyuncs.com/apis/apphub/data.json",
+        {}
+      ).then((data) => {
+        resolve((data as Result).list);
       });
-    },
-    {
-			manual: true,
-      onSuccess: (res) => {
-        console.log("请求结果", res);
-				message.success('请求成功')
-      },
-    }
-  );
+    }) as Promise<DataType[]>;
+  });
+
+	console.log("data111", data);
 
   return (
-    <>
-      <div className="m-3">
-        <Button loading={fetchLoading} onClick={handleFakeFetch}>
-          点击请求
-        </Button>
+    <div className="h-screen overflow-hidden flex flex-col">
+      <div className="flex items-center h-12 px-4">
+        {/* <img
+          src="https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/common/24385370.jpeg"
+          className="inline-block h-8 w-8 rounded-2xl"
+        /> */}
+        <div className="font-semibold text-lg">Apphub</div>
       </div>
-      <div className="m-3">
-        <Button>
-          <a href="/react-template/about">路由切换</a>
-        </Button>
+      <div className="px-4">
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item, index) => (
+            <List.Item
+              className="cursor-pointer"
+              onClick={() => {
+                window.open(item.url);
+              }}
+              actions={[
+                <div className="flex items-center text-[#1677ff]">
+                  前往 <RightOutlined className="ml-1" />
+                </div>,
+              ]}
+            >
+              <List.Item.Meta
+                avatar={<Avatar src={item.icon} />}
+                title={
+                  <a href={item.url} target="_blank">
+                    {item.title}
+                  </a>
+                }
+                description={item.description}
+              />
+            </List.Item>
+          )}
+        />
       </div>
-      <div className="m-3">
-        <Button>
-          <a href="/react-template/antd-demo">AntD Demo</a>
-        </Button>
-      </div>
-    </>
+      {/* <div className="flex flex-wrap p-4 items-start">
+        {data?.map((item) => {
+					return (
+            <div className="rounded-t-xl mr-2 w-28 overflow-hidden mb-8"
+							style={{
+								fontSize: 0
+							}}
+						>
+              <img className="inline-block w-full" src={item.icon} />
+              <div className='border-t-0 border border-solid border-[#ddd] rounded-b-xl'>
+                <div className="font-semibold p-2 ellipsis-single text-base">
+                  {item.title}
+                </div>
+                <div className="ellipsis-single p-2 text-sm">
+                  {item.description}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div> */}
+    </div>
   );
 }
 
