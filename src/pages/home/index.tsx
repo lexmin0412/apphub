61b4317da81294a1a060f98dbc5b8f2a27bfe365
lexmin0412 <zhangle_dev@outlook.com>
2023-11-29 23:23:25 +0800

    fix: 修复 lint 问题

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,5 @@
-import {Button, message} from "antd";
-import {useEffect} from "react";
 import fetch from "@toolkit-fe/request";
 import {useRequest} from "ahooks";
-import {Card} from "antd";
-const {Meta} = Card;
 import {Avatar, List} from "antd";
 import { RightOutlined }  from '@ant-design/icons'
 
@@ -20,7 +16,7 @@ interface Result {
 }
 
 function Home() {
-  const {loading: fetchLoading, data} = useRequest(() => {
+  const { data} = useRequest(() => {
     return new Promise((resolve) => {
       fetch(
         "https://lexmin.oss-cn-hangzhou.aliyuncs.com/apis/apphub/data.json",
@@ -36,17 +32,13 @@ function Home() {
   return (
     <div className="h-screen overflow-hidden flex flex-col">
       <div className="flex items-center h-12 px-4">
-        {/* <img
-          src="https://lexmin.oss-cn-hangzhou.aliyuncs.com/statics/common/24385370.jpeg"
-          className="inline-block h-8 w-8 rounded-2xl"
-        /> */}
         <div className="font-semibold text-lg">Apphub</div>
       </div>
       <div className="px-4">
         <List
           itemLayout="horizontal"
           dataSource={data}
-          renderItem={(item, index) => (
+          renderItem={(item) => (
             <List.Item
               className="cursor-pointer"
               onClick={() => {
@@ -71,27 +63,6 @@ function Home() {
           )}
         />
       </div>
-      {/* <div className="flex flex-wrap p-4 items-start">
-        {data?.map((item) => {
-					return (
-            <div className="rounded-t-xl mr-2 w-28 overflow-hidden mb-8"
-							style={{
-								fontSize: 0
-							}}
-						>
-              <img className="inline-block w-full" src={item.icon} />
-              <div className='border-t-0 border border-solid border-[#ddd] rounded-b-xl'>
-                <div className="font-semibold p-2 ellipsis-single text-base">
-                  {item.title}
-                </div>
-                <div className="ellipsis-single p-2 text-sm">
-                  {item.description}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div> */}
     </div>
   );
 }
